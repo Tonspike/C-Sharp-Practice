dd15604776d7fea538a305fdc9ec9345d3e5d476
Kristen Thayer <kristenmthayer@gmail.com>
2016-03-29 16:53:31 -0700

    initial commit

---
+++ Coding Practice/Factory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Practice
+{
+    class Factory
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
---
+++ Coding Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory.SayHello();
+        }
+    }
+}
