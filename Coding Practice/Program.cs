794361eba986fa6c5a87b6ded03fb97d16c90800
Kristen Thayer <kristenmthayer@gmail.com>
2016-04-19 14:02:05 -0700

    solved the first part of the problem, still playing with how to complete the rest, but have to take a break

--- Coding Practice/Program.cs
+++ Coding Practice/Program.cs
@@ -10,27 +10,50 @@ namespace Coding_Practice
     {
         static void Main(string[] args)
         {
-            var stack = Factory.StackPractice();
-            foreach (int i in stack)
+            //problem: given a stack of unknown size, reverse that stack using only one other stack and a variable.
+            var stack1 = Factory.StackPractice();
+            Console.WriteLine("first stack:");
+            foreach (int i in stack1)
             {
                 Console.WriteLine(i);
             }
-            var queue = Factory.QueuePractice();
-            foreach (int i in queue)
+            Stack<int> stack2 = new Stack<int>();
+            int count = 0;
+            foreach (int i in stack1)
+            {
+                //get an accurate count of how many elements have to be moved
+                count++;
+            }
+            //for (int j = 0; j< count; j++)//repeat this process as many times as count.
+            //{
+                int temp = stack1.Pop();//save the top item in the variable, then push the rest through.
+                foreach (int i in stack1)
+                {
+                    stack2.Push(i);
+                }
+                for (int i = 1; i < count; i++)
+                {
+                    stack1.Pop(); //empty first stack, ready for new input
+                }
+                stack1.Push(temp); //temp number is now at the bottom of stack, push the rest in after it:
+                foreach (int i in stack2)
+                {
+                    stack1.Push(i);
+                }
+            for (int i = 1; i < count; i++)
+            {
+                stack2.Pop(); //empty first stack, ready for new input
+            }
+            //  }
+            Console.WriteLine("second stack:");
+            foreach (int i in stack1)
             {
                 Console.WriteLine(i);
             }
-            var qnum = queue.Dequeue();
-            var snum = stack.Pop();
+           
+            
 
-            foreach (int i in stack)
-            {
-                Console.WriteLine(i);
-            }
-            foreach (int i in queue)
-            {
-                Console.WriteLine(i);
-            }
+
         }
     }
 }
